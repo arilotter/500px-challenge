c9f302b05fd8172859dc01c83a66fc391d9b2fbe
arilotter <arilotter@gmail.com>
2017-02-18 13:38:43 -0500

    Moved API logic from App to PhotoGrid

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Toolbar from './components/Toolbar';
 import PhotoGrid from './components/PhotoGrid';
 import './App.css';
 
-class App extends Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      photos: [],
-      page: 0
-    };
-    this.loadPage = this.loadPage.bind(this); // So we can use 'this' in the method
-  }
+const App = () => (
+  <div className='container'>
+    <Toolbar />
+    <PhotoGrid />
+  </div>
+);
 
-  componentWillMount () {
-    this.loadPage();
-  }
-
-  loadPage () {
-    window.fetch('/api/popular/' + this.state.page)
-      .then(res => res.json())
-      .then(({page, photos}) => {
-        if (page <= this.state.page) {
-          return console.error('Tried to load an already-loaded page.');
-        }
-        this.setState({
-          photos: [...this.state.photos, ...photos],
-          page: page
-        });
-      });
-  }
-
-  render () {
-    return (
-      <div>
-        <Toolbar />
-        <PhotoGrid photos={this.state.photos} />
-      </div>
-    );
-  }
-}
 
 export default App;
--- client/src/components/PhotoGrid/index.js
+++ client/src/components/PhotoGrid/index.js
@@ -1,20 +1,46 @@
-import React from 'react';
-import './style.css';
+import React, { Component } from 'react';
 
+import './style.css';
 import PhotoTile from '../PhotoTile';
 
 
-const PhotoGrid = ({ photos }) => {
-  const photoTiles = photos.map(photo => (
-    <PhotoTile
-      title={photo.title}
-      thumbnailUrl={photo.thumbnailUrl}
-      key={photo.id}
-    />
-  ));
-  return (
-    <div>{photoTiles}</div>
-  );
-};
+export default class PhotoGrid extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      photos: [],
+      page: 0
+    };
+    this.loadPage = this.loadPage.bind(this); // So we can use 'this' in the method
+  }
+  componentWillMount () {
+    this.loadPage();
+  }
 
-export default PhotoGrid;
+  loadPage () {
+    window.fetch('/api/popular/' + this.state.page)
+      .then(res => res.json())
+      .then(({page, photos}) => {
+        if (page <= this.state.page) {
+          return console.error('Tried to load an already-loaded page.');
+        }
+        this.setState({
+          photos: [...this.state.photos, ...photos],
+          page: page
+        });
+      });
+  }
+
+  render () {
+    const photoTiles = this.state.photos.map(photo => (
+      <PhotoTile
+        title={photo.title}
+        thumbnailUrl={photo.thumbnailUrl}
+        key={photo.id}
+      />
+    ));
+    return (
+      <div className='photoGrid'>{photoTiles}</div>
+    );
+  }
+}
