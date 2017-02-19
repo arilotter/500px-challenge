40bf753b8950905f43207275f8d47e8cbb67b9a8
arilotter <arilotter@gmail.com>
2017-02-19 16:11:00 -0500

    Created a CSS animation for when photos load

--- client/src/components/PhotoTile/index.js
+++ client/src/components/PhotoTile/index.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import classNames from 'classnames';
 import './style.css';
 
-const PhotoTile = (props) => {
-  return (
-    <img
-      className='photoTile'
-      src={props.thumbnailUrl}
-      alt={props.title}
-      width={props.width}
-      height={props.height}
-    />
-  );
-};
+export default class PhotoTile extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      loaded: false
+    };
+  }
 
-export default PhotoTile;
+  render () {
+    return (
+      <img
+        className={classNames('photoTile', { 'photoTileLoaded': this.state.loaded })} // Run the animation once the tile has loaded
+        src={this.props.thumbnailUrl}
+        alt={this.props.title}
+        width={this.props.width}
+        height={this.props.height}
+        onLoad={() => this.setState({ loaded: true })}
+      />
+    );
+  }
+}
