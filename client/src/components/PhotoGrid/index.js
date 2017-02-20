e1698bcd058bbd69aeaed5575607da7d7df680f0
arilotter <arilotter@gmail.com>
2017-02-19 21:53:22 -0500

    Support orientation changes

--- client/src/components/PhotoGrid/index.js
+++ client/src/components/PhotoGrid/index.js
@@ -5,38 +5,46 @@ import Spinner from '../Spinner';
 import './style.css';
 
 const GUTTER_WIDTH = 16;
-const PHOTO_WIDTH = Math.min((window.screen.width - GUTTER_WIDTH * 3) / 2, 300); // Scale images based on maximum window size (for mobile)
 
 export default class PhotoGrid extends Component {
   constructor (props) {
     super(props);
     this.state = {
-      photos: []
+      photos: [],
+      photoIDs: [],
+      ...this.calculatePhotoWidth()
     };
-    this.loadMore = this.loadMore.bind(this); // So we can use 'this' in the method
-    // columns
-    this.sizes = [...Array(25).keys()].map(i => { // Support filling up to 8k monitors :)
+    // Bind class methods to 'this' so we can refer to the instance in the method
+    this.loadMore = this.loadMore.bind(this);
+    this.calculatePhotoWidth = this.calculatePhotoWidth.bind(this);
+
+    window.addEventListener('orientationchange', () => {
+      this.calculatePhotoWidth();
+    });
+  }
+
+  calculatePhotoWidth () {
+    const photoWidth = Math.min((window.screen.availWidth - GUTTER_WIDTH * 3) / 2, 300); // Scale images based on maximum window size (for mobile)
+    const columnBreakpoints = [...Array(25).keys()].map(i => { // Support filling up to 8k monitors :)
       const columns = i + 1;
-      const mq = i > 0 ? (columns + 1) * GUTTER_WIDTH + columns * PHOTO_WIDTH + 'px' : undefined; // the smallest breakpoint shouldn't have a specified size
+      const mq = i > 0 ? (columns + 1) * GUTTER_WIDTH + columns * photoWidth + 'px' : undefined; // the smallest breakpoint shouldn't have a specified size
       return { mq, columns, gutter: GUTTER_WIDTH };
     });
-    this.photoIDs = [];
+    return { photoWidth, columnBreakpoints };
   }
 
   loadMore (newPage) {
     window.fetch('/api/popular/' + newPage)
       .then(res => res.json())
-      .then(({loadedPage, photos}) => {
-        if (loadedPage <= newPage) {
+      .then(({page: loadedPage, photos: loadedPhotos}) => {
+        if (loadedPage < newPage) {
           return console.error('Tried to load an already-loaded page.');
         }
         // Eliminate any duplicates
-        const newPhotos = photos.filter(photo => !this.photoIDs.includes(photo.id));
-        // Keep track of all photo IDs
-        this.photoIDs = [...this.photoIDs, ...newPhotos.map(photo => photo.id)];
-        this.setState({
-          photos: [...this.state.photos, ...newPhotos]
-        });
+        const photos = [...this.state.photos, ...loadedPhotos.filter(photo => !this.state.photoIDs.includes(photo.id))];
+        // Keep track of all new photo IDs
+        const photoIDs = photos.map(photo => photo.id);
+        this.setState({ photos, photoIDs });
       });
   }
 
@@ -46,18 +54,18 @@ export default class PhotoGrid extends Component {
         className='photoGrid'
         pageStart={0}
         loadMore={this.loadMore}
-        sizes={this.sizes}
+        sizes={this.state.columnBreakpoints}
         loader={<Spinner />}
         hasMore
       >
         {
           this.state.photos.map(photo => {
-            const calculatedHeight = Math.round(PHOTO_WIDTH / photo.thumbWidth * photo.thumbHeight);
+            const calculatedHeight = Math.round(this.state.photoWidth / photo.thumbWidth * photo.thumbHeight);
             return (<PhotoTile
               key={photo.id}
               title={photo.title}
               thumbnailUrl={photo.thumbnailUrl}
-              width={PHOTO_WIDTH}
+              width={this.state.photoWidth}
               height={calculatedHeight}
             />);
           })
